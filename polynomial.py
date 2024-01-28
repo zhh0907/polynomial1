5365d76d31df08c9f3cf1c894d00390eabb38786
zhh0907 <zhh0907@bu.edu>
2024-01-27 22:01:54 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -13,6 +13,8 @@ class X:
     def __repr__(self):
         return "X"
     
+    def evaluate(self, x_value):
+        return x_value
 
 class Int:
     def __init__(self, i):
@@ -21,6 +23,8 @@ class Int:
     def __repr__(self):
         return str(self.i)
     
+    def evaluate(self, x_value):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -30,6 +34,9 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) + self.p2.evaluate(x_value)
+    
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -43,7 +50,10 @@ class Mul:
         if isinstance(self.p2, (Add, Div, Sub)):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
-
+    
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) * self.p2.evaluate(x_value)
+    
 class Div:
     def __init__(self, p1, p2):
         if p2 == 0 or (isinstance(p2, Int) and p2.i == 0):
@@ -60,7 +70,11 @@ class Div:
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
     
-
+    def evaluate(self, x_value):
+        divider = self.p2.evaluate(x_value)
+        if divider == 0:
+            raise Exception("Cannot divide by zero")
+        return self.p1.evaluate(x_value) / divider
     
 class Sub:
     def __init__(self, p1, p2):
@@ -75,10 +89,12 @@ class Sub:
         if isinstance(self.p2, (Add, Mul, Div)):
             return repr(self.p1) + " - ( " + repr(self.p2) + " )"
         return repr(self.p1) + " - " + repr(self.p2)
-
+    
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) - self.p2.evaluate(x_value)
 
         
 
 
-poly = Add( Add( Int(4), Int(3)), Add( X(), Div( Int(1),  Int(0))))
-print(poly)
+poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly.evaluate(-1))
